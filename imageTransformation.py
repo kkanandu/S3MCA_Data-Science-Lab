3fa2ca5162bea088f52f805581899c337ec9b922
Anandu kk <116473698+kkanandu@users.noreply.github.com>
2023-10-04 22:48:58 +0530

    Update imageTransformation.py

--- imageTransformation.py
+++ imageTransformation.py
@@ -13,4 +13,11 @@ b=ndimage.rotate(img,200)
 plt.title("Rotated Image")
 plt.imshow(b)
 
+# d=(img.size[0] // 2,img.size[1] // 2)
+# c=img.resize(d)
+# plt.title("zoom Image")
+# plt.imshow(c)
 
+d=ndimage.zoom(a,(.3))
+plt.imshow(d)
+plt.title("zoom Image")
